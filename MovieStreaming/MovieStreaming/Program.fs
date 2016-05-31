af62f9493c5e761ac9caf9da51bc19938925abe7
Thomas Jaskula <thomasv1000@hotmail.fr>
2016-05-31 14:04:21 +0200

    Add actor lifecycle sample

--- MovieStreaming/MovieStreaming/Program.fs
+++ MovieStreaming/MovieStreaming/Program.fs
@@ -1,5 +1,6 @@
 ﻿open Akka.Actor
 open Akka.FSharp
+open ComposeIt.Akka.FSharp.Extensions.Lifecycle
 open System
 
 open Actors
@@ -36,6 +37,8 @@ let main argv =
     actor1 <! "Akka.NET : The Movie"
     actor1 <! 42
 
+    Console.ReadKey() |> ignore
+
     (* 
     =======
     Typed
@@ -71,6 +74,8 @@ let main argv =
     actor2 <! {MovieTitle = "Codenan the Destroyer"; UserId = 1}
     actor2 <! 87
     actor2 <! PoisonPill.Instance
+
+    Console.ReadKey() |> ignore
     
     // 2'# actor
     let props = Props.Create<PlaybackActorTyped>()
@@ -82,6 +87,37 @@ let main argv =
     actor2' <! {MovieTitle = "Codenan the Destroyer"; UserId = 1}
     actor2' <! 48 // Unhandled method is called itself on typed actor
     actor2' <! PoisonPill.Instance
+
+    Console.ReadKey() |> ignore
+
+    // 3# actor, overriding lifecycle
+
+    let preStart = Some(fun (baseFn : unit -> unit) -> cprintfn ConsoleColor.Green "Playback Actor 3 PreStart")
+    let postStop = Some(fun (baseFn : unit -> unit) -> cprintfn ConsoleColor.Green "Playback Actor 3 PostStop")
+    let preRestart = Some(fun (baseFn : exn * obj -> unit) -> cprintfn ConsoleColor.Green "Playback Actor PreRestart because: %A" exn)
+    let postRestart = Some(fun (baseFn : exn -> unit) -> cprintfn ConsoleColor.Green "Playback Actor PostRestart because: %A" exn)
+
+    let actor3 = 
+        spawnOvrd system "PlaybackActor3"
+        <| fun mailbox ->
+            cprintfn ConsoleColor.Gray "Creating the actor 3..."
+            let rec loop() = actor {                
+                let! (msg : PlayMovieMessage) = mailbox.Receive()
+                match msg with
+                    | m when m.UserId > 40 -> cprintfn ConsoleColor.Yellow "Received movie title %s and User ID %i" m.MovieTitle m.UserId
+                    | _ -> cprintfn ConsoleColor.Red "Unhadled message..."
+                           mailbox.Unhandled msg
+                return! loop()
+            }
+            loop()
+        <| {defOvrd with PreStart = preStart; PostStop = postStop}
+
+    actor3 <! {MovieTitle = "Akka.NET : The Movie"; UserId = 42}
+    actor3 <! {MovieTitle = "Partial Recall"; UserId = 99}
+    actor3 <! {MovieTitle = "Boolean Lies"; UserId = 77}
+    actor3 <! {MovieTitle = "Codenan the Destroyer"; UserId = 1}
+    actor3 <! 87
+    actor3 <! PoisonPill.Instance
 
     Console.ReadKey() |> ignore
 
