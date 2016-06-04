73dc5311c8d5f50ff9c675bd59bb469ca8975b3c
Jaskula Tomasz <thomasv1000@hotmail.fr>
2016-06-05 01:06:43 +0200

    Add 5th actor

--- MovieStreaming/MovieStreaming/Program.fs
+++ MovieStreaming/MovieStreaming/Program.fs
@@ -218,6 +218,65 @@ let main argv =
 
     Console.ReadKey() |> ignore
 
+    // 5# user actor
+    Console.WriteLine(Environment.NewLine)
+    cprintfn ConsoleColor.Magenta "Starting new actor..."
+
+    let preStart = Some(fun (baseFn : unit -> unit) -> cprintfn ConsoleColor.Green "UserActor PreStart")
+    let postStop = Some(fun (baseFn : unit -> unit) -> cprintfn ConsoleColor.Green "UserActor PostStop")
+    let preRestart = Some(fun (baseFn : exn * obj -> unit) -> cprintfn ConsoleColor.Green "UserActor PreRestart because: %A" exn)
+    let postRestart = Some(fun (baseFn : exn -> unit) -> cprintfn ConsoleColor.Green "UserActor PostRestart because: %A" exn)
+
+    let actor5 = 
+        spawnOvrd system "UserActorBecome"
+        <| fun mailbox ->
+            cprintfn ConsoleColor.Gray "Creating the actor 5..."
+            let rec loop lastState = actor {                
+                let! (msg : obj) = mailbox.Receive()
+
+                let handlePlayMovieMessage (message : PlayMovieMessage) : string =
+                    match lastState with
+                    | null | "" -> cprintfn ConsoleColor.Yellow "User is currently watching %s" message.MovieTitle
+                                   message.MovieTitle
+                    | t -> cprintfn ConsoleColor.Red "Error: cannot start playing another movie before stopping existing one"
+                           lastState
+
+                let handleStopMovieMessage (message : StopMovieMessage) : string =
+                    match lastState with
+                    | null | "" -> cprintfn ConsoleColor.Red "Error: cannot stop if nothing is playing"
+                                   lastState
+                    | _ -> cprintfn ConsoleColor.Yellow "User has stopped watching %s" lastState
+                           String.Empty
+
+                let newState = match msg with
+                               | :? PlayMovieMessage as pmm -> handlePlayMovieMessage pmm
+                               | :? StopMovieMessage as smm -> handleStopMovieMessage smm
+                               | _ -> cprintfn ConsoleColor.Red "Unhadled message..."
+                                      mailbox.Unhandled msg
+                                      ""
+                return! loop newState
+            }
+            loop String.Empty
+        <| {defOvrd with PreStart = preStart; PostStop = postStop; PreRestart = preRestart; PostRestart = postRestart}
+
+    Console.ReadKey() |> ignore
+    cprintfn ConsoleColor.Blue "Sending a PlayMovieMessage (Codenan the Destroyer)" 
+    actor5 <! {MovieTitle = "Codenan the Destroyer"; UserId = 42}
+    
+    Console.ReadKey() |> ignore
+    cprintfn ConsoleColor.Blue "Sending a PlayMovieMessage (Boolean Lies)" 
+    actor5 <! {MovieTitle = "Boolean Lies"; UserId = 42}
+
+    Console.ReadKey() |> ignore
+    cprintfn ConsoleColor.Blue "Sending a StopMovieMessage" 
+    actor5 <! StopMovieMessage()
+
+    Console.ReadKey() |> ignore
+    cprintfn ConsoleColor.Blue "Sending a another StopMovieMessage" 
+    actor5 <! StopMovieMessage()
+
+    Console.ReadKey() |> ignore
+
     // 5'# user actor
     Console.WriteLine(Environment.NewLine)
     cprintfn ConsoleColor.Magenta "Starting new actor..."
