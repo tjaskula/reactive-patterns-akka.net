cb82b726819251be6fe248a460cc7f34f5777d04
Jaskula Tomasz <thomasv1000@hotmail.fr>
2016-06-06 22:35:03 +0200

    Add functional Message definition

--- MovieStreaming/MovieStreaming/Messages.fs
+++ MovieStreaming/MovieStreaming/Messages.fs
@@ -3,3 +3,8 @@
     type PlayMovieMessage = {MovieTitle : string; UserId : int}
 
     type StopMovieMessage () = class end
+
+    // more functional definition
+    type Message =
+        | PlayMovie of PlayMovieMessage
+        | StopMovie of StopMovieMessage
