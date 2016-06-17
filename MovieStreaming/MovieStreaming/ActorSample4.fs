24662561d7ef337841077cb64d8416e55500e08d
Thomas Jaskula <thomasv1000@hotmail.fr>
2016-06-17 18:25:10 +0200

    Move Actor4 to it's own file

---
+++ MovieStreaming/MovieStreaming/ActorSample4.fs
@@ -0,0 +1,73 @@
+﻿module Actor4
+
+open Akka.Actor
+open Akka.FSharp
+open System
+
+open Actors
+open ComposeIt.Akka.FSharp.Extensions.Lifecycle
+open ConsoleHelpers
+open Messages
+
+let start6 system =
+    // 4# user actor
+    Console.WriteLine(Environment.NewLine)
+    cprintfn ConsoleColor.Magenta "Starting new actor..."
+
+    let preStart = Some(fun (baseFn : unit -> unit) -> cprintfn ConsoleColor.Green "UserActor PreStart")
+    let postStop = Some(fun (baseFn : unit -> unit) -> cprintfn ConsoleColor.Green "UserActor PostStop")
+    let preRestart = Some(fun (baseFn : exn * obj -> unit) -> cprintfn ConsoleColor.Green "UserActor PreRestart because: %A" exn)
+    let postRestart = Some(fun (baseFn : exn -> unit) -> cprintfn ConsoleColor.Green "UserActor PostRestart because: %A" exn)
+
+    let actor4 = 
+        spawnOvrd system "UserActor"
+        <| fun mailbox ->
+            cprintfn ConsoleColor.Gray "Creating the actor 4..."
+            let rec loop lastState = actor {                
+                let! (msg : obj) = mailbox.Receive()
+
+                let handlePlayMovieMessage (message : PlayMovieMessage) : string =
+                    match lastState with
+                    | null | "" -> cprintfn ConsoleColor.Yellow "User is currently watching %s" message.MovieTitle
+                                   message.MovieTitle
+                    | t -> cprintfn ConsoleColor.Red "Error: cannot start playing another movie before stopping existing one"
+                           lastState
+
+                let handleStopMovieMessage (message : StopMovieMessage) : string =
+                    match lastState with
+                    | null | "" -> cprintfn ConsoleColor.Red "Error: cannot stop if nothing is playing"
+                                   lastState
+                    | _ -> cprintfn ConsoleColor.Yellow "User has stopped watching %s" lastState
+                           String.Empty
+
+                let newState = match msg with
+                               | :? PlayMovieMessage as pmm -> handlePlayMovieMessage pmm
+                               | :? StopMovieMessage as smm -> handleStopMovieMessage smm
+                               | _ -> cprintfn ConsoleColor.Red "Unhadled message..."
+                                      mailbox.Unhandled msg
+                                      ""
+                return! loop newState
+            }
+            loop String.Empty
+        <| {defOvrd with PreStart = preStart; PostStop = postStop; PreRestart = preRestart; PostRestart = postRestart}
+
+    Console.ReadKey() |> ignore
+    cprintfn ConsoleColor.Blue "Sending a PlayMovieMessage (Codenan the Destroyer)" 
+    actor4 <! {MovieTitle = "Codenan the Destroyer"; UserId = 42}
+    
+    Console.ReadKey() |> ignore
+    cprintfn ConsoleColor.Blue "Sending a PlayMovieMessage (Boolean Lies)" 
+    actor4 <! {MovieTitle = "Boolean Lies"; UserId = 42}
+
+    Console.ReadKey() |> ignore
+    cprintfn ConsoleColor.Blue "Sending a StopMovieMessage" 
+    actor4 <! StopMovieMessage()
+
+    Console.ReadKey() |> ignore
+    cprintfn ConsoleColor.Blue "Sending a another StopMovieMessage" 
+    actor4 <! StopMovieMessage()
+
+    Console.ReadKey() |> ignore
+
+
+    
--- MovieStreaming/MovieStreaming/UseCases.fs
+++ MovieStreaming/MovieStreaming/UseCases.fs
@@ -3,10 +3,11 @@
 open Actor1
 open Actor2
 open Actor3
+open Actor4
 open Akka.Actor
 
 let (config : System.Collections.Generic.IDictionary<int, ActorSystem -> unit>) = 
-    dict[(1, start1); (2, start2); (3, start3); (4, start4); (5, start5)] 
+    dict[(1, start1); (2, start2); (3, start3); (4, start4); (5, start5); (6, start6)] 
 
 let startActor system number =
     let (success, startfn) = config.TryGetValue(number)
