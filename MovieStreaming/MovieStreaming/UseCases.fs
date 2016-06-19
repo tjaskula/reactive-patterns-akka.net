3ea9a492e411411d5fbe251f3b781f50bef9323f
Thomas Jaskula <thomasv1000@hotmail.fr>
2016-06-19 18:33:48 +0200

    Move Actor 5 to it's own file

--- MovieStreaming/MovieStreaming/UseCases.fs
+++ MovieStreaming/MovieStreaming/UseCases.fs
@@ -4,10 +4,11 @@ open Actor1
 open Actor2
 open Actor3
 open Actor4
+open Actor5
 open Akka.Actor
 
 let (config : System.Collections.Generic.IDictionary<int, ActorSystem -> unit>) = 
-    dict[(1, start1); (2, start2); (3, start3); (4, start4); (5, start5); (6, start6); (7, start7)] 
+    dict[(1, start1); (2, start2); (3, start3); (4, start4); (5, start5); (6, start6); (7, start7); (8, start8)] 
 
 let startActor system number =
     let (success, startfn) = config.TryGetValue(number)
