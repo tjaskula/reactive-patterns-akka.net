f8059f916e4f178a36d869904938872ae3b371fa
Jaskula Tomasz <thomasv1000@hotmail.fr>
2017-04-22 16:28:05 +0200

    Adjusting Example 3

--- src/Basic Lifecycle and States/MovieStreaming/MovieStreaming/ActorSample3.fs
+++ src/Basic Lifecycle and States/MovieStreaming/MovieStreaming/ActorSample3.fs
@@ -20,19 +20,24 @@ let start5 system =
     let postRestart = Some(fun (baseFn : exn -> unit) -> cprintfn ConsoleColor.Green "Playback Actor PostRestart because: %A" exn)
 
     let actor3 = 
-        spawnOvrd system "PlaybackActor3"
+        spawn system "PlaybackActor3"
         <| fun mailbox ->
             cprintfn ConsoleColor.Gray "Creating the actor 3..."
             let rec loop() = actor {                
-                let! (msg : PlayMovieMessage) = mailbox.Receive()
+                let! msg = mailbox.Receive()
                 match msg with
-                    | m when m.UserId > 40 -> cprintfn ConsoleColor.Yellow "Received movie title %s and User ID %i" m.MovieTitle m.UserId
+                    | LifecycleEvent e ->
+                        match e with
+                        | PreStart -> cprintfn ConsoleColor.Green "Playback Actor 3 PreStart"
+                        | PostStop -> cprintfn ConsoleColor.Green "Playback Actor 3 PostStop"
+                        | PreRestart (exn, _) -> cprintfn ConsoleColor.Green "Playback Actor PreRestart because: %A" exn
+                        | PostRestart exn -> cprintfn ConsoleColor.Green "Playback Actor PostRestart because: %A" exn
+                    | m as PlayMovieMessage when m.UserId > 40 -> cprintfn ConsoleColor.Yellow "Received movie title %s and User ID %i" m.MovieTitle m.UserId
                     | _ -> cprintfn ConsoleColor.Red "Unhadled message..."
                            mailbox.Unhandled msg
                 return! loop()
             }
             loop()
-        <| {defOvrd with PreStart = preStart; PostStop = postStop; PreRestart = preRestart; PostRestart = postRestart}
 
     actor3 <! {MovieTitle = "Akka.NET : The Movie"; UserId = 42}
     actor3 <! {MovieTitle = "Partial Recall"; UserId = 99}
